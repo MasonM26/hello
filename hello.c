149020b5f2879b9d72ece20d0e8593ec1918bc68
Mason <mm766317@southeast.edu>
2025-08-28 09:50:42 -0500

    initial comit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Mason Melcher
+ *         mmelcher4@huskers.unl.edu
+ * Date: 2025/26/08
+ *
+ * A simple program that print my name in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Mason Melcher\n");
+
+
+  return 0;
+}
+
